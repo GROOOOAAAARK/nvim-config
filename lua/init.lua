3397069dada1069736fd1e87b5cc0d961ba0f983
thomasAtCoppengo <thomas.lenoir@coopengo.com>
2023-10-13 18:08:14 +0200

    feat(dap): add base config in comment
    still need to review and test

--- lua/init.lua
+++ lua/init.lua
@@ -1,3 +1,4 @@
+-- Mason
 require("mason").setup({
     ui = {
         icons = {
@@ -8,6 +9,8 @@ require("mason").setup({
     }
 })
 
+
+-- LSP configs
 require("mason-lspconfig").setup({
     ensure_installed = { "lua-ls", "tsserver", "tailwindcss", "pyright", "terraformls", "bashls", "rust_analyzer" }--, "sonarlint"-- }
 })
@@ -37,3 +40,25 @@ require'lspconfig'.tsserver.setup{}
 --   }
 --})
 
+
+-- Debugger DAP config (uses vscode debugger)
+-- require('dap.ext.vscode').load_launchjs(nil, {})
+-- local dap, dapui =require("dap"),require("dapui")
+-- dap.listeners.after.event_initialized["dapui_config"]=function()
+--   dapui.open()
+-- end
+-- dap.listeners.before.event_terminated["dapui_config"]=function()
+--   dapui.close()
+-- end
+-- dap.listeners.before.event_exited["dapui_config"]=function()
+--   dapui.close()
+-- end
+--
+-- vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
+-- vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
+-- vim.keymap.set('n', '<F5>', require 'dap'.continue)
+-- vim.keymap.set('n', '<F10>', require 'dap'.step_over)
+-- vim.keymap.set('n', '<F11>', require 'dap'.step_into)
+-- vim.keymap.set('n', '<F12>', require 'dap'.step_out)
+-- vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint)
+--
